6d211ade65690d53ddd7d53260d5e1bd6ec7b1f7
Doru Musuroi <doru.musuroi@gmail.com>
2017-02-24 10:54:20 +0200

    Deleted unnecessary openface demo folder

--- Demo 3 - Training a Classifier - OpenFace_files/extra.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-  var snowplowTracker = Snowplow.getTrackerUrl('joule.isr.cs.cmu.edu:8081');
-  snowplowTracker.enableLinkTracking();
-  snowplowTracker.trackPageView();
-}
