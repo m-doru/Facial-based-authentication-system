136fa424da10a5fb2155741663fe9e89533c238f
Doru Musuroi <doru.musuroi@gmail.com>
2017-03-31 08:32:29 +0300

    Compute feature for spoof classification on red channel

--- Implementation/faceSpoofDetection/SVMclassifier/feature_computer.py
+++ Implementation/faceSpoofDetection/SVMclassifier/feature_computer.py
@@ -8,6 +8,9 @@ class FrameFeatureComputer:
     def compute_features(self, frame):
         small_frame = cv2.resize(frame, features.FRAME_SIZE)
 
-        grey_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
+        #gray image
+        #matrix = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
+        #red channel
+        matrix = small_frame[:,:,2]
 
-        return self.feature_computer.compute(grey_small_frame)
+        return self.feature_computer.compute(matrix)
