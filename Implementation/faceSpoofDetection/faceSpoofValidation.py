02e9bb5efa78146083ba4dcaf7bccd9a4d572c77
Doru Musuroi <doru.musuroi@gmail.com>
2017-04-24 11:26:21 +0300

    Use red channel for image validation

--- Implementation/faceSpoofDetection/faceSpoofValidation.py
+++ Implementation/faceSpoofDetection/faceSpoofValidation.py
@@ -2,30 +2,32 @@ from __future__ import print_function
 import cv2
 import features
 import pickle
+import joblib
 
 
 class FaceSpoofValidator:
     def __init__(self, lbp, path_to_classifier):
         self._lbp = lbp
         self._sift = features.DSIFT()
-        #self.clf = joblib.load(path_to_classifier)
-        with open(path_to_classifier, 'r') as f:
-            self.clf = pickle.load(f)
+        self.clf = joblib.load(path_to_classifier)
 
     def validate_face(self, face):
         print(face.shape)
 
         face = cv2.resize(face, features.FRAME_SIZE)
-        grey_aligned_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        #matrix = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        matrix = face[:,:,2]
+        
 
-        lbp_feature = self._lbp.compute(grey_aligned_face)
-        #dsift_feature = self._sift.compute(grey_aligned_face, 8, 16)
+        lbp_feature = self._lbp.compute(matrix)
+        #dsift_feature = self._sift.compute(matrix, 8, 16)
 
         feature_vector = lbp_feature
 
-        print(self.clf.predict([feature_vector]))
+        print(self.clf.predict_proba([feature_vector]))
+        pred_confidence = self.clf.predict_proba([feature_vector])
 
-        if self.clf.predict([feature_vector]) == 1:
+        if pred_confidence[0][0] > 0.5:
             return True
         else:
             return False
