a92d10c24e502d4392575f3ec882f51731dbb90a
Doru Musuroi <doru.musuroi@gmail.com>
2017-05-28 08:43:24 +0300

    Progress on faces distance threshold learning

--- Implementation/facesDistanceThreshold/main.py
+++ Implementation/facesDistanceThreshold/main.py
@@ -6,10 +6,12 @@ from face import Face
 import itertools
 import os
 import re
+import time
 
 import joblib
 import matplotlib.pyplot as pyplot
 import matplotlib.patches as mpatches
+
 
 def get_faces_with_ids(path_to_db, verbose = False):
     face_id_multiple_pattern = re.compile(".+[1-9]+\.[a-z]{3}")
@@ -40,12 +42,13 @@ def get_faces_with_ids(path_to_db, verbose = False):
     return faces
 
 def get_face_pairs(faces, examples_per_subject):
+    take_all = examples_per_subject <= 0
     filtered_faces = []
     used_faces = {}
     for face in faces:
         if face.id not in used_faces:
             used_faces[face.id] = 1
-        if used_faces[face.id] < examples_per_subject:
+        if used_faces[face.id] < examples_per_subject or take_all:
             filtered_faces.append(face)
             used_faces[face.id] += 1
 
@@ -58,33 +61,66 @@ def get_face_pairs(faces, examples_per_subject):
 
     return face_pairs
 
+def get_faces_distances(faces, examples_per_subject):
+    take_all = examples_per_subject <= 0
+    filtered_faces = []
+    used_faces = {}
+    for face in faces:
+        if face.id not in used_faces:
+            used_faces[face.id] = 1
+        if used_faces[face.id] < examples_per_subject or take_all:
+            filtered_faces.append(face)
+            used_faces[face.id] += 1
 
-def main(load=False, examples_per_subject=None):
-    saved_faces_filename = "sorted_face_pairs.pkl"
-    if not load:
-        path_to_db = '../databases/MsCelebsV1-Faces-Cropper-07'
-        faces = get_faces_with_ids(path_to_db, True)
-        joblib.dump(faces, saved_faces_filename)
-    else:
-        faces = joblib.load(saved_faces_filename)
-
-    face_pairs = get_face_pairs(faces, examples_per_subject)
     same_id_distances = []
     diff_id_distances = []
-    for fp in face_pairs:
-        if fp.same_id:
-            same_id_distances.append(fp.distance)
+    for (face1, face2) in itertools.combinations(filtered_faces, 2):
+        if face1.id == face2.id:
+            same_id_distances.append(face1.compute_distance(face2.representation))
         else:
-            diff_id_distances.append(fp.distance)
+            diff_id_distances.append(face1.compute_distance(face2.representation))
 
-    pyplot.figure(1)
-    pyplot.hist(same_id_distances, 40, normed=1, facecolor='green')
+    return (same_id_distances, diff_id_distances)
+
+def main(load_faces=False, load_distances=False, examples_per_subject=None):
+    saved_faces_filename = "sorted_face_pairs.pkl"
+    if load_faces is not None:
+        if not load_faces:
+            #path_to_db = '../databases/MsCelebsV1-Faces-Cropper-07'
+            path_to_db = '/home/doru/Desktop/MsCelebV1-Faces-Aligned.Samples'
+            faces = get_faces_with_ids(path_to_db, True)
+            joblib.dump(faces, saved_faces_filename)
+        else:
+            faces = joblib.load(saved_faces_filename)
+            print("Faces loaded")
+
+
+    saved_distances_same_id_filename = "same_id_distances_"+str(examples_per_subject)+".pkl"
+    saved_distances_diff_id_filename = "diff_id_distances_"+str(examples_per_subject)+".pkl"
+
+    if not load_distances or not (os.path.exists(saved_distances_same_id_filename)
+                                or os.path.exists(saved_distances_diff_id_filename)):
+        (same_id_distances, diff_id_distances) = get_faces_distances(faces, examples_per_subject)
+        print("Computed distances")
+        joblib.dump(same_id_distances, saved_distances_same_id_filename)
+        print("Saved same id distances")
+        joblib.dump(diff_id_distances, saved_distances_diff_id_filename)
+        print("Saved different id distances")
+    else:
+        with open(saved_distances_same_id_filename, 'rb') as f:
+            same_id_distances = joblib.load(f)
+            print("Loaded same id distances")
+        with open(saved_distances_diff_id_filename, 'rb') as f:
+            diff_id_distances = joblib.load(f)
+            print("Loaded different id distances")
+
+    (n_s, _, _) = pyplot.hist(same_id_distances, 40, normed=True,facecolor='green')
+    (n_d, _, _) = pyplot.hist(diff_id_distances,40, normed=True, facecolor='red', alpha=0.75)
     pyplot.title('Distribution of face distances')
     pyplot.xlabel('Faces distance')
     pyplot.ylabel('# of face pairs')
-    pyplot.axis([0, 4, 0, 1])
+    pyplot.axis([0, 4, 0, max([max(n_s), max(n_d)])])
     pyplot.grid(True)
-    pyplot.hist(diff_id_distances, 40, normed=1, facecolor='red', alpha=0.75)
 
     diff_subj_patch = mpatches.Patch(color='red', label='Different subjects')
     same_subj_patch = mpatches.Patch(color='green', label='Same subjects')
@@ -94,4 +130,6 @@ def main(load=False, examples_per_subject=None):
     pyplot.savefig('dist_faces_' + str(examples_per_subject) + '.png')
 
 if __name__ == "__main__":
-    main(True, 10)
+    start = time.time()
+    main(True, False,10)
+    print("Processing took {}".format(time.time() - start))
